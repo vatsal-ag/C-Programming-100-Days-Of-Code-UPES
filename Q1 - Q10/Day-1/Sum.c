f65bef6f4684e1419bd4a602d0e8e36fbf0c454e
vatsal-ag <117094352+vatsal-ag@users.noreply.github.com>
2025-08-26 19:56:53 +0530

    Day 1: 100 Days of Code

---
+++ Q1 - Q10/Day-1/Sum.c
@@ -0,0 +1,10 @@
+// Q1: Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+int main() 
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("Sum = %d\n", a + b);
+    return 0;
+}
