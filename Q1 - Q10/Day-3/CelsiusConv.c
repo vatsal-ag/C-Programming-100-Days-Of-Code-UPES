e3b41021ee71aed2c67d7aa5f15b9637f482fdc8
Vatsal Agarwal <vatsalagarwal.2001@gmail.com>
2025-08-31 02:42:42 +0530

    Add files via upload

---
+++ Q1 - Q10/Day-3/CelsiusConv.c
@@ -0,0 +1,11 @@
+//Write a program to convert temperature from Celsius to Fahrenheit.
+
+#include <stdio.h>
+int main() 
+{
+    float c;
+    scanf("%f", &c);
+    float f = (c * 9/5) + 32;
+    printf("Fahrenheit=%.2f\n", f);
+    return 0;
+}
---
+++ Q11 - Q20/Day-7/LeapYear.c
@@ -0,0 +1,15 @@
+// Write a program to input a year and check whether it is a leap year or not using conditional statements.
+
+#include <stdio.h>
+int main()
+{
+    int year;
+    scanf("%d", &year);
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+        printf("Leap year\n");
+    else
+        printf("Not a leap year\n");
+
+    return 0;
+}
