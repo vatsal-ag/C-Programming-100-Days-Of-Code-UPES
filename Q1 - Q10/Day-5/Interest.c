195232eefc1402fcaeba0f09a0e12e55e7b9c187
Vatsal Agarwal <vatsalagarwal.2001@gmail.com>
2025-08-31 17:43:05 +0530

    Update Interest.c

--- Q1 - Q10/Day-5/Interest.c
+++ Q1 - Q10/Day-5/Interest.c
@@ -1,4 +1,6 @@
 //Write a program to calculate simple and compound interest for given principal, rate, and time.
+//Name: Vatsal Agarwal
+//SAP ID: 590023223 
 
 #include <stdio.h>
 #include <math.h>
