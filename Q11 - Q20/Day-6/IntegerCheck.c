7f4e56a89ec0643ec937bde33cf44c231838b6b6
Vatsal Agarwal <vatsalagarwal.2001@gmail.com>
2025-08-31 17:44:17 +0530

    Update IntegerCheck.c

--- Q11 - Q20/Day-6/IntegerCheck.c
+++ Q11 - Q20/Day-6/IntegerCheck.c
@@ -1,4 +1,6 @@
 //Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+//Name: Vatsal Agarwal
+//SAP ID: 590023223 
 
 #include <stdio.h>
 int main() 
