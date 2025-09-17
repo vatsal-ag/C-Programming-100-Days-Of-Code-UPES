531c86c59dc1b2e73ee378ccf0f6b4fb41f1377b
vatsal-ag <117094352+vatsal-ag@users.noreply.github.com>
2025-09-17 19:57:06 +0530

    Day 23: 100 Days of Code

---
+++ Q41 - Q50/Day-23/Pattern.c
@@ -0,0 +1,26 @@
+// Write a program to print the following pattern:
+/*
+*****
+*****
+*****
+*****
+*****
+*/
+// Name: Vatsal Agarwal
+// SAP ID: 590023223
+
+#include <stdio.h>
+
+int main() 
+{
+    int i, j;
+    for(i = 1; i <= 5; i++) 
+    {
+        for(j = 1; j <= 5; j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
